9f25a4373894a7a2e9492336ee492562cdefd887
juanlp21 <55576203+juanlp21@users.noreply.github.com>
2020-02-27 09:26:25 +0100

    Add files via upload

---
+++ Myfirstproject_Java/src/HelloWorld.java
@@ -0,0 +1,17 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int cont,numero=5;
+		System.out.println("Hello world");
+		for(cont=0;cont<=10;cont++){
+			System.out.print (cont);
+			System.out.print (" x ");
+			System.out.print (numero);
+			System.out.print (" = ");
+			System.out.println (cont*numero);
+		}
+	}
+
+}
